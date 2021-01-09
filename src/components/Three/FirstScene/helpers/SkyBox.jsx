ed8df074676f3e995045522987b6aea847192190
blewis11 <bejallewis@hotmail.com>
2021-01-09 18:35:43 +0000

    new skybox

--- src/components/Three/FirstScene/helpers/SkyBox.jsx
+++ src/components/Three/FirstScene/helpers/SkyBox.jsx
@@ -9,14 +9,14 @@ const SkyBox = ({ skyboxHeight }) => {
   for (let i = 0; i < 6; i++)
     materialArray.push(
       new THREE.MeshLambertMaterial({
-        map: THREE.ImageUtils.loadTexture('scene1_backgroundv4/' + directions[i]),
+        map: THREE.ImageUtils.loadTexture('scene1_backgroundv5/' + directions[i]),
         side: THREE.BackSide,
       }),
     )
 
   let skyBox = new THREE.Mesh(skyGeometry, materialArray)
 
-  return <primitive object={skyBox} rotation={[0, 3.1, 0]} position={[0, -6, 0]} />
+  return <primitive object={skyBox} rotation={[0, 3.1, 0]} position={[0, -3, 0]} />
 }
 
 export { SkyBox }
