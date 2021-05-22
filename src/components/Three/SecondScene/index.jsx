a379ba08cf2eabe203a12ac392d55870386e8a1f
blewis11 <bejallewis@hotmail.com>
2021-05-22 12:26:03 +0200

    make room larger and add sky light

--- src/components/Three/SecondScene/index.jsx
+++ src/components/Three/SecondScene/index.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from 'react-three-fiber'
 import { Provider } from 'react-redux'
 import * as THREE from 'three'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib'
 import { Html, OrbitControls } from 'drei'
 import CameraControls from "camera-controls";
 
@@ -41,20 +43,34 @@ const App = ({ state, setState }) => {
 
     return (
         <DatGui data={state.data} onUpdate={handleUpdate}>
-            <DatNumber path='positionX' label='positionX' min={-50} max={50} step={1} />
-            <DatNumber path='positionY' label='positionY' min={-50} max={50} step={1} />
-            <DatNumber path='positionZ' label='positionZ' min={-50} max={50} step={1} />
+            <DatNumber path='positionX' label='positionX' min={-200} max={200} step={1} />
+            <DatNumber path='positionY' label='positionY' min={-200} max={200} step={1} />
+            <DatNumber path='positionZ' label='positionZ' min={-200} max={200} step={1} />
             <DatNumber path='rotation' label='rotation' min={-5} max={5} step={0.1} />
         </DatGui>
     )
 }
 
+const RectLight = ({ state }) => {
+    RectAreaLightUniformsLib.init()
+    const rectLight1 = new THREE.RectAreaLight(0xfdf3c6, 0.5, 120, 120);
+    const helper = new RectAreaLightHelper(rectLight1)
+
+    return (
+        <>
+            <group position={[0, 17, -25]} rotation={[- Math.PI / 2, 0, 0]}>
+                <primitive object={rectLight1} />
+                <primitive object={helper} />
+            </group>
+        </>
+    )
+}
 
 const Floor = () => {
     const planeRef = useRef()
     const [state, setState] = useState({
         data: {
-            positionX: 0, positionY: -11, positionZ: -25, rotation: 0.9
+            positionX: 31, positionY: -3, positionZ: -5, rotation: 0.9
         }
     })
 
@@ -65,39 +81,40 @@ const Floor = () => {
             <group position={[0, 7, 26]}>
                 {/* bottom  */}
                 <mesh position={[0, -11, -25]} ref={planeRef} rotation={[- Math.PI / 2, 0, 0]}>
-                    <circleGeometry attach="geometry" args={[15, 5]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
-                </mesh>
-                {/* back */}
-                <mesh position={[-4, -4, -37]} ref={planeRef} rotation={[0, 0.3, 0]}>
-                    <planeGeometry attach="geometry" args={[20, 20, 100, 32]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
-                </mesh>
-                {/* back left */}
-                <mesh position={[-12, -4.1, -25]} ref={planeRef} rotation={[0, 1.6, 0]}>
-                    <planeGeometry attach="geometry" args={[20, 20, 100, 32]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                    <circleGeometry attach="geometry" args={[45, 5]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
                 </mesh>
                 {/* back right */}
-                <mesh position={[8, -4, -35]} ref={planeRef} rotation={[0, -0.95, 0]}>
-                    <planeGeometry attach="geometry" args={[25, 20, 100, 32]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                <mesh position={[-14, -4, -59]} ref={planeRef} rotation={[0, 0.3, 0]}>
+                    <planeGeometry attach="geometry" args={[60, 35, 100, 32]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                </mesh>
+                {/* back left */}
+                <mesh position={[-34, -4, -25]} ref={planeRef} rotation={[0, 1.6, 0]}>
+                    <planeGeometry attach="geometry" args={[60, 35, 100, 32]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                </mesh>
+                {/* back right */}
+                <mesh position={[29, -4, -48]} ref={planeRef} rotation={[0, -0.95, 0]}>
+                    <planeGeometry attach="geometry" args={[60, 35, 100, 32]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
                 </mesh>
                 {/* front left */}
-                <mesh position={[-7, -4.1, -15]} ref={planeRef} rotation={[0, 2.8, 0]}>
-                    <planeGeometry attach="geometry" args={[25, 20, 100, 32]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                <mesh position={[-7, -4, 12]} ref={planeRef} rotation={[0, 2.8, 0]}>
+                    <planeGeometry attach="geometry" args={[60, 35, 100, 32]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
                 </mesh>
                 {/* front right */}
-                <mesh position={[9, -2, -17]} ref={planeRef} rotation={[0, 0.9, 0]}>
-                    <planeGeometry attach="geometry" args={[25, 20, 100, 32]} />
-                    <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
+                <mesh position={[31, -4, -5]} ref={planeRef} rotation={[0, 0.9, 0]}>
+                    <planeGeometry attach="geometry" args={[60, 35, 100, 32]} />
+                    <meshStandardMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
                 </mesh>
-                {/* bottom  */}
-                <mesh position={[0, 5, -25]} ref={planeRef} rotation={[- Math.PI / 2, 0, 0]}>
+                {/* top  */}
+                <RectLight state={state} />
+                {/* <mesh position={[0, 5, -25]} ref={planeRef} rotation={[- Math.PI / 2, 0, 0]}>
                     <circleGeometry attach="geometry" args={[15, 5]} />
                     <meshPhongMaterial attach="material" color={0xd9b1e2} side={THREE.DoubleSide} />
-                </mesh>
+                </mesh> */}
             </group>
         </>
     )
@@ -108,7 +125,7 @@ const SecondScene = ({ store }) => {
         <>
             <Canvas colorManagement shadowMap camera={{ position: [0, 0, EPS], fov: 80 }}>
                 <Provider store={store}>
-                    <pointLight color={"purple"} intensity={0.5} />
+                    {/* <pointLight color={"purple"} intensity={0.5} /> */}
                     <Floor />
 
                     <WithCameraControlers />
